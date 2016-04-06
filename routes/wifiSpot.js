22b3694ef49967b320ff2b0bb695dc455105f79f
Rodthung Sudrung <r.sudrung@gmail.com>
2016-04-06 08:52:11 -0700

    add like incremental

--- routes/wifiSpot.js
+++ routes/wifiSpot.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var wifiSpot = require('../models/wifiSpot')
+var mongodb = require('mongodb')
 
 router.route('/')
 
-.post(function(req, res) {
-    
+.post(function (req, res) {
+
     var wifi = new wifiSpot();
-    
+
     wifi.name = req.body.name;
     wifi.lat = req.body.lat;
     wifi.long = req.body.long;
@@ -15,21 +16,47 @@ router.route('/')
     wifi.wifiPass = req.body.wifiPass;
     wifi.likes = req.body.likes;
     wifi.unlikes = req.body.unlikes;
-    
-    wifi.save(function(err) {
-        if(err)
+
+    wifi.save(function (err) {
+        if (err)
             res.send(err);
-        res.json({ message: 'wifiSpot saved'});
+        res.json({
+            message: 'wifiSpot saved'
+        });
     });
 })
 
-.get(function(req, res) {
-    
-    wifiSpot.find(function(err, wifies){
-        if(err)
+.get(function (req, res) {
+
+    wifiSpot.find(function (err, wifies) {
+        if (err)
             res.send(err);
         res.json(wifies);
-    });    
+    });
 });
+
+router.route('/like/:wifiId')
+
+.post(function (req, res) {
+
+    wifiSpot.findOne({
+        _id: new mongodb.ObjectID(req.params.wifiId)
+    }, function (err, wifiSpot) {
+
+        if (err)
+            res.send(err);
+
+        wifiSpot.likes++;
+        wifiSpot.save(function (err) {
+            if (err)
+                res.send(err);
+            res.json({
+                message: 'wifiSpot like plus one'
+            });
+        });
+    });
+})
+
+
 
 module.exports = router;
