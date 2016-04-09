4eda037042827f5d91fa7a3541c5fa17424486cb
Rodthung Sudrung <r.sudrung@gmail.com>
2016-04-09 13:02:54 -0700

    no message

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var router = express.Router();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://salty-tor-54109.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://salty-tor-54109.herokuapp.com/api/wifiSpot');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
